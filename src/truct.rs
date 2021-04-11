75b808127332de7a2458c5754949459b99de02e4
ivan-boginski <aisivan36@gmail.com>
2021-04-11 10:31:48 +0700

    modified

--- src/truct.rs
+++ src/truct.rs
@@ -1,3 +1,8 @@
+pub fn main_truct() {
+    main();
+    // anEnum();
+    main_enum();
+}
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -19,7 +24,8 @@ struct Rectangle {
     buttom_right: Point,
 }
 
-pub fn main_truct() {
+// anEnum();
+fn main() {
     // Create sturc with field init shorthand
     let name = String::from("Ivan");
     let age = 27;
@@ -46,7 +52,7 @@ pub fn main_truct() {
         y: left_edge,
     } = point;
 
-    let _rectangle = Rectangle {
+    let _whatever = Rectangle {
         // struct instantiation is an expression too
         top_left: Point {
             x: left_edge,
@@ -68,4 +74,50 @@ pub fn main_truct() {
     let Pair(integer, decimal) = pair;
 
     println!("pair contains {:?} and {:?}", integer, decimal);
+}
+
+// Create an `enum` to classify a web event. Note how both
+// names and type information together specify the variant:
+// `PageLoad != PageUnload` and `KeyPress(char) != Paste(String)`.
+// Each is different and independent.
+enum WebEvent {
+    // An `enum` may either be `unit-like`,
+    PageLoad,
+    PageUnload,
+    // like tuple structs,
+    KeyPress(char),
+    Paste(String),
+    // or c-like structures.
+    Click { x: i64, y: i64 },
+}
+
+// A function which takes a `WebEvent` enum as an argument and
+// returns nothing.
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        // Destructure `c` from inside the `enum`.
+        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+        // Destructure `Click` into `x` and `y`.
+        WebEvent::Click { x, y } => {
+            println!("clicked at x={}, y={}.", x, y);
+        }
+    }
+}
+
+fn main_enum() {
+    let pressed = WebEvent::KeyPress('x');
+    // `to_owned()` creates an owned `String` from a string slice.
+    let pasted = WebEvent::Paste("my text".to_owned());
+    let click = WebEvent::Click { x: 20, y: 80 };
+    let load = WebEvent::PageLoad;
+    let unload = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
 }
